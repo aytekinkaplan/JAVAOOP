2c68230e8aee8c9c9a61614a781d851e157138e6
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-10-02 07:24:24 +0300

    Commit

--- src/Inheritance/ExamplePack001/Class004.java
+++ src/Inheritance/ExamplePack001/Class004.java
@@ -3,7 +3,7 @@ package Inheritance.ExamplePack001;
 public class Class004 {
     public static void main(String[] args) {
         Class003 aPerson = new Class003("Tedddy","Bear",4000,"Teacher",18,"ANGARA");
-
+        System.out.println(aPerson.name);
 
 
     }
